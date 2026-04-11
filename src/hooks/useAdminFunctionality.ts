850d1a2b36afe40c032ababb9fff7b5b1a5626ad
MAS40se7en <manir.saker@gmail.com>
2026-04-11 22:43:52 +0200

    32.05: ANOTHER LITTLE FIX, dev indicators and functionality removed for live integration

--- src/hooks/useAdminFunctionality.ts
+++ src/hooks/useAdminFunctionality.ts
@@ -447,7 +447,7 @@ export const useAdminFunctionality = () => {
   const createCoupon = useMutation({
     mutationKey: ['create-coupon'],
     mutationFn: async (formData: Partial<Tables<'coupons'>>) => {
-      const { data: couponData, error: couponError } = await supabase.functions.invoke('create-coupons-with-stripe-dev', {
+      const { data: couponData, error: couponError } = await supabase.functions.invoke('create-coupons-with-stripe', {
         body: JSON.stringify({ formData })
       });
 
