e4d7df1acf9cbd6cd5c42379bd493afd73dfd8bb
MAS40se7en <manir.saker@gmail.com>
2026-06-01 22:33:48 +0200

    41:Small steps

---
+++ src/components/AiComponents/MessageInputField.tsx
@@ -0,0 +1,7 @@
+const MessageInputField = () => {
+  return (
+    <div>MessageInputField</div>
+  )
+}
+
+export default MessageInputField
---
+++ src/components/AiComponents/MessagesBox.tsx
@@ -0,0 +1,11 @@
+interface Props {
+    className?: string
+}
+
+const MessagesBox = ({ className }: Props) => {
+  return (
+    <div className={className}>MessagesBox</div>
+  )
+}
+
+export default MessagesBox
